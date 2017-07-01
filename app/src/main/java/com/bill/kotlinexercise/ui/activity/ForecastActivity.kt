de4aa2c7652bffdbb0a260a91ca3e60f27d2c2c6
weibiao <weibiao@17shihui.com>
2017-07-01 20:54:43 +0800

    网络获取天气展示到recycleView

--- app/src/main/java/com/bill/kotlinexercise/ui/activity/ForecastActivity.kt
+++ app/src/main/java/com/bill/kotlinexercise/ui/activity/ForecastActivity.kt
@@ -2,13 +2,16 @@ package com.bill.kotlinexercise.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.bill.kotlinexercise.R
+import com.bill.kotlinexercise.domain.Forecast
 import com.bill.kotlinexercise.domain.RequestForecastCommand
 import com.bill.kotlinexercise.ui.adapter.ForecastListAdapter
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.find
+import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 
 class ForecastActivity : AppCompatActivity() {
@@ -23,10 +26,15 @@ class ForecastActivity : AppCompatActivity() {
     private fun initView() {
         val forecastList: RecyclerView = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
         doAsync {
             val result = RequestForecastCommand("94043").execute()
             uiThread {
-                forecastList.adapter = ForecastListAdapter(result)
+                forecastList.adapter = ForecastListAdapter(result, object : ForecastListAdapter.OnItemClickListener {
+                    override fun invoke(forecast: Forecast) {
+                        toast(forecast.data)
+                    }
+                })
             }
         }
     }
--- app/src/main/java/com/bill/kotlinexercise/ui/adapter/ForecastListAdapter.kt
+++ app/src/main/java/com/bill/kotlinexercise/ui/adapter/ForecastListAdapter.kt
@@ -1,17 +1,26 @@
 package com.bill.kotlinexercise.ui.adapter
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.bill.kotlinexercise.R
+import com.bill.kotlinexercise.domain.Forecast
 import com.bill.kotlinexercise.domain.ForecastList
+import com.squareup.picasso.Picasso
+import org.jetbrains.anko.find
 
 /**
  * Created by Bill on 2017/6/15.
  */
-class ForecastListAdapter(val weekForecast: ForecastList) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+class ForecastListAdapter(val weekForecast: ForecastList, val itemClick: OnItemClickListener) :
+        RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(TextView(parent.context))
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_forecast, parent, false)
+        return ViewHolder(view, itemClick)
     }
 
     override fun getItemCount(): Int {
@@ -19,11 +28,41 @@ class ForecastListAdapter(val weekForecast: ForecastList) : RecyclerView.Adapter
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        with(weekForecast[position]) {
-            holder.textView.text = "$data - $description - $high/$low"
-        }
+        holder.bindForecast(weekForecast[position])
     }
 
-    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    class ViewHolder(view: View, val itemClick: OnItemClickListener) : RecyclerView.ViewHolder(view) {
+        private val iconView: ImageView
+        private val dateView: TextView
+        private val descriptionView: TextView
+        private val maxTemperatureView: TextView
+        private val minTemperatureView: TextView
+
+        init {
+            iconView = view.find(R.id.icon)
+            dateView = view.find(R.id.date)
+            descriptionView = view.find(R.id.description)
+            maxTemperatureView = view.find(R.id.maxTemperature)
+            minTemperatureView = view.find(R.id.minTemperature)
+        }
+
+        fun bindForecast(forecast: Forecast) {
+            with(forecast) {
+                Picasso.with(itemView.context).load(iconUrl).into(iconView)
+                dateView.text = data
+                descriptionView.text = description
+                maxTemperatureView.text = high.toString()
+                minTemperatureView.text = low.toString()
+                itemView.setOnClickListener {
+                    itemClick.invoke(forecast)
+                }
+            }
+        }
+
+    }
+
+    interface OnItemClickListener {
+        fun invoke(forecast: Forecast)
+    }
 
 }
